f5693dfad082e30aa35e5562112f9d958fc04ef5
Nina Shirole <ninashirole@uchicago.edu>
2021-07-06 03:20:20 +0000

    array element square

--- array_element_square.rb
+++ array_element_square.rb
@@ -11,3 +11,7 @@ p "Enter at least 2 numbers, separated by spaces:"
 user_string = gets.chomp
 
 user_numbers = user_string.split
+user_numbers_length = user_numbers.count
+penultimate_index = user_numbers_length - 2
+penultimate_value = user_numbers[penultimate_index]
+puts penultimate_value.to_f ** 2
